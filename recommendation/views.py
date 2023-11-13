113e41d70078c046790ea05bf0909d3e584e33ec
sypark <sypark132@gmail.com>
2023-11-13 23:26:14 +0900

    recommendation/view.py 수정 필요

--- recommendation/views.py
+++ recommendation/views.py
@@ -1,35 +1,82 @@
 from django.shortcuts import render
-#from .recommendation import recommend_size  # 딥러닝 모델을 사용할 모듈
+from .utils import recommend_size 
 from .forms import SizeRecommendationForm
 # Create your views here.
 import logging
 
+# 수정 필요 
+# 폼 받지 않고 return redirect('/recommendation')으로 호출 받았을때 실행되게 변경 필요
 def recommendation(request):
     if request.method == 'POST':
         form = SizeRecommendationForm(request.POST)
-        if form.is_valid():
-            gender = form.cleaned_data['gender']
-            height = form.cleaned_data['height']
-            weight = form.cleaned_data['weight']
-            clothing_type = form.cleaned_data['clothing_type']
-            logging.warning('1.여기서 문제다.', height)
+        
+        predict_top = request.session.get('predict_top', 0)
+        predict_chest = request.session.get('predict_chest', 0)
+        predict_shoulder = request.session.get('predict_shoulder', 0)
+        predict_arm = request.session.get('predict_arm', 0)
+        predict_neck = request.session.get('predict_neck', 0)
+        predict_ntk = request.session.get('predict_ntk', 0)
+        predict_waist = request.session.get('predict_waist', 0)
+        predict_ass = request.session.get('predict_ass', 0)
+        predict_bottom = request.session.get('predict_bottom', 0)
+        predict_thighs = request.session.get('predict_thighs', 0)
+        clothing_type = request.session.get('clothing_type')
+                    
+            # 예측한 사용자 신체 치수 정보
+            
+        print(f"predict_top: {predict_top}")
+        # 상의
+        if clothing_type in ['outer', 'top']:
 
-            additional_info = {}  # 옷 종류에 따른 추가 정보
-            if clothing_type in ['outer', 'top']:
-                additional_info['shoulder'] = form.cleaned_data['shoulder']
-                additional_info['chest'] = form.cleaned_data['chest']
-                additional_info['total_length'] = form.cleaned_data['total_length']
-                additional_info['sleeve'] = form.cleaned_data['sleeve']
-            elif clothing_type in ['bottom', 'skirt']:
-                additional_info['waist'] = form.cleaned_data['waist']
-                additional_info['hip'] = form.cleaned_data['hip']
-                additional_info['bottom_length'] = form.cleaned_data['bottom_length']
-                additional_info['thigh'] = form.cleaned_data['thigh']
+            clothes_shoulder = request.session.get('clothes_shoulder', 0)
+            clothes_chest = request.session.get('clothes_chest', 0)
+            clothes_total_length = request.session.get('clothes_total_length', 0)
+            clothes_sleeve = request.session.get('clothes_sleeve', 0)
+
+            diff_shoulder = abs(predict_shoulder - clothes_shoulder)
+            diff_chest = abs(predict_chest - clothes_chest)
+            diff_total_length = abs(predict_top - clothes_total_length)
+            diff_sleeve = abs(predict_arm - clothes_sleeve)
             
-            # 딥러닝 모델을 사용하여 사이즈 추천
-           # recommended_size = recommend_size(gender, height, weight, clothing_type, additional_info)
+            request.session['diff_shoulder'] = diff_shoulder
+            request.session['diff_chest'] = diff_chest
+            request.session['diff_total_length'] = diff_total_length
+            request.session['diff_sleeve'] = diff_sleeve
+                
+        # 하의
+        elif clothing_type in ['bottom', 'skirt']:
+
+            clothes_waist = request.session.get('clothes_waist', 0)
+            clothes_hip = request.session.get('clothes_hip', 0)
+            clothes_bottom_length = request.session.get('clothes_bottom_length', 0)
+            clothes_thigh = request.session.get('clothes_thigh', 0)
             
-            # 추천된 사이즈를 사용자에게 표시
-           # return render(request, 'recommendation_result.html', {'recommended_size': recommended_size})
+            diff_waist = abs(predict_waist - clothes_waist)
+            diff_hip = abs(predict_ass - clothes_hip)
+            diff_bottom_length = abs(predict_bottom - clothes_bottom_length)
+            diff_thigh = abs(predict_thighs - clothes_thigh)
+            
+            request.session['diff_waist'] = diff_waist
+            request.session['diff_hip'] = diff_hip
+            request.session['diff_bottom_length'] = diff_bottom_length
+            request.session['diff_thigh'] = diff_thigh
+                    
+        recommend_size(request)
+        
+        result_data = {
+            'diff_shoulder': request.session.get('diff_shoulder'),
+            'diff_chest': request.session.get('diff_chest'),
+            'diff_total_length': request.session.get('diff_total_length'),
+            'diff_sleeve': request.session.get('diff_sleeve'),
+            'diff_waist': request.session.get('diff_waist'),
+            'diff_hip': request.session.get('diff_hip'),
+            'diff_bottom_length': request.session.get('diff_bottom_length'),
+            'diff_thigh': request.session.get('diff_thigh'),
+        }
+        return render(request, 'recommendation/result.html', result_data)
+
+
     else:
         form = SizeRecommendationForm()
+        
+    return render(request, 'recommendation/result.html', {'form': form})
