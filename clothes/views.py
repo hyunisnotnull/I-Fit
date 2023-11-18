8f613e81c7439201fcd9d384b8262c28f713d222
sypark729 <148840329+sypark729@users.noreply.github.com>
2023-11-18 20:00:49 +0900

    Update views.py

--- clothes/views.py
+++ clothes/views.py
@@ -13,7 +13,10 @@ def clothes(request):
         print( request.session.get('user_height') )
 
         if form.is_valid():
+            print("Form is valid")
+            print(form.cleaned_data)
             clothing_type = form.cleaned_data['clothing_type']
+            request.session['clothing_type'] = clothing_type
             # 선택한 옷의 종류에 따라 필요한 옷 치수 입력을 요구하거나 입력된 데이터를 처리합니다.
             if clothing_type in ['outer', 'top']:
                 # 상의 및 아우터의 치수 입력 필드가 비어 있지 않다면, 해당 데이터를 처리
@@ -21,26 +24,52 @@ def clothes(request):
                 chest = form.cleaned_data['chest']
                 total_length = form.cleaned_data['total_length']
                 sleeve = form.cleaned_data['sleeve']
-                # 이어서 데이터 처리 코드를 작성
                 # 데이터 저장 추가
                 request.session['shoulder'] = shoulder
-                request.session['chest'] = chest
+                request.session['chest'] = chest * 2
                 request.session['total_length'] = total_length
                 request.session['sleeve'] = sleeve
 
             elif clothing_type in ['bottom', 'skirt']:
                 # 바지 및 치마의 치수 입력 필드가 비어 있지 않다면, 해당 데이터를 처리
-                waist = form.cleaned_data['waist']
+                waist = form.cleaned_data['waist'] 
                 hip = form.cleaned_data['hip']
                 bottom_length = form.cleaned_data['bottom_length']
                 thigh = form.cleaned_data['thigh']
 
-                request.session['waist'] = waist
-                request.session['hip'] = hip
+                request.session['waist'] = waist * 2
+                request.session['hip'] = hip * 2
                 request.session['bottom_length'] = bottom_length
-                request.session['thigh'] = thigh
+                request.session['thigh'] = thigh * 2
+                
+            elif clothing_type in ['long']:
+                
+                shoulder = form.cleaned_data['shoulder']
+                chest = form.cleaned_data['chest']
+                total_length = form.cleaned_data['total_length']
+                sleeve = form.cleaned_data['sleeve']
+                
+                request.session['shoulder'] = shoulder
+                request.session['chest'] = chest * 2
+                request.session['total_length'] = total_length
+                request.session['sleeve'] = sleeve
 
-            return redirect('recommendation')
+
+            elif clothing_type in ['shirt']:
+                shoulder = form.cleaned_data['shoulder']
+                chest = form.cleaned_data['chest']
+                total_length = form.cleaned_data['total_length']
+                sleeve = form.cleaned_data['sleeve']
+                neck = form.cleaned_data['neck']
+                
+                request.session['shoulder'] = shoulder
+                request.session['chest'] = chest * 2
+                request.session['total_length'] = total_length
+                request.session['sleeve'] = sleeve
+                request.session['neck'] = neck
+
+            print(form.cleaned_data)
+            return redirect('/recommendation')
         print(f"Session data after recommend_size: {request.session}")
     else:
 
